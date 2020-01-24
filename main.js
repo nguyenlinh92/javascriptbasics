d0d8bca2c9155247af4017e5dfaf4210aebd8ec0
Nguyen Linh <nguyentrandl92@gmail.com>
2020-01-25 00:11:11 +0100

    conditions

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log('Hello');
-var name = 'text';
-var number = 0234;
+var name = 'Honza';
+var number = 0.234;
 var jeBlbej = true;
 console.log(name);
 
@@ -11,4 +11,10 @@ function cook(mleko, mouka, vejce) {
     console.log('varim ' + mleko + ' ' + mouka + ' ' + vejce);
 }
 cook(mleko, mouka, vejce);
-
+if (number>0) {
+    console.log('pět je větší než nula');
+    console.log('deset je větší jak pět');
+}
+if (name === 'Honza') {
+    console.log('Jmenuje se Honza');
+}
