e5083a979f71e93c53ced36116725a87ecc7393f
Nguyen Linh <nguyentrandl92@gmail.com>
2020-01-24 23:55:40 +0100

    zaklad javascriptu
    zakladni typy a funkce

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log('Hello');
+var name = 'text';
+var number = 0234;
+var jeBlbej = true;
+console.log(name);
+
+var mleko = 'mleko';
+var mouka = 'mouka';
+var vejce = 'vejce';
+function cook(mleko, mouka, vejce) {
+    console.log('varim ' + mleko + ' ' + mouka + ' ' + vejce);
+}
+cook(mleko, mouka, vejce);
+
