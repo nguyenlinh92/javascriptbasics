3de494f32600f79ca3724aa1c5d45514c270fd6b
Nguyen Linh <nguyentrandl92@gmail.com>
2020-03-06 22:40:05 +0100

    lekce2

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
-console.log('Hello');
-var name = 'Honza';
-var number = 0.234;
-var jeBlbej = true;
-console.log(name);
+// console.log('Hello');
+// var name = 'Honza';
+// var number = 0.234;
+// var jeBlbej = true;
+// console.log(name);
 
-var mleko = 'mleko';
-var mouka = 'mouka';
-var vejce = 'vejce';
-function cook(mleko, mouka, vejce) {
-    console.log('varim ' + mleko + ' ' + mouka + ' ' + vejce);
+// var mleko = 'mleko';
+// var mouka = 'mouka';
+// var vejce = 'vejce';
+function cook(firstIngredient, secondIngredient, thirdIngredient) {
+    // if (firstIngredient !== mleko) {
+    //     console.log('chybi' + ' ' + mleko)
+    // } else if (secondIngredient !== mouka) {
+    //     console.log('chybi' + ' ' + mouka)
+    // } else if (thirdIngredient !== vejce) {
+    // console.log('chybi' + ' ' + vejce)    
+    // } else {
+    // console.log('Mam vsechny ingredience.')    
+    // }
+    if (firstIngredient !== "mleko") {
+        console.log('chybi' + ' ' + mleko)
+    }
+    if (secondIngredient !== "mouka") {
+        console.log('chybi' + ' ' + "mouka")
+    }
+    if (thirdIngredient !== "vejce") {
+        console.log('chybi' + ' ' + vejce)
+    }
+    console.log('varim ' + firstIngredient + ' ' + secondIngredient + ' ' + thirdIngredient);
 }
-cook(mleko, mouka, vejce);
-if (number>0) {
-    console.log('pět je větší než nula');
-    console.log('deset je větší jak pět');
-}
-if (name === 'Honza') {
-    console.log('Jmenuje se Honza');
-}
+// cook(mleko, mouka, vejce)
+// cook();
+
+// if (number>0) {
+    // console.log('pět je větší než nula');
+    // console.log('deset je větší jak pět');}
+// if (name === 'Honza') {
+    // console.log('Jmenuje se Honza');}
