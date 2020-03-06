0e833c20989384b57399dbfb6b4bdf62f57ef0dd
Nguyen Linh <nguyentrandl92@gmail.com>
2020-03-06 23:14:51 +0100

    functions

--- main.js
+++ main.js
@@ -1,38 +1,26 @@
-// console.log('Hello');
-// var name = 'Honza';
-// var number = 0.234;
-// var jeBlbej = true;
-// console.log(name);
-
-// var mleko = 'mleko';
-// var mouka = 'mouka';
-// var vejce = 'vejce';
-function cook(firstIngredient, secondIngredient, thirdIngredient) {
-    // if (firstIngredient !== mleko) {
-    //     console.log('chybi' + ' ' + mleko)
-    // } else if (secondIngredient !== mouka) {
-    //     console.log('chybi' + ' ' + mouka)
-    // } else if (thirdIngredient !== vejce) {
-    // console.log('chybi' + ' ' + vejce)    
-    // } else {
-    // console.log('Mam vsechny ingredience.')    
-    // }
-    if (firstIngredient !== "mleko") {
-        console.log('chybi' + ' ' + mleko)
-    }
-    if (secondIngredient !== "mouka") {
-        console.log('chybi' + ' ' + "mouka")
-    }
-    if (thirdIngredient !== "vejce") {
-        console.log('chybi' + ' ' + vejce)
-    }
-    console.log('varim ' + firstIngredient + ' ' + secondIngredient + ' ' + thirdIngredient);
+function count(firstNum, secondNum) {
+    console.log(firstNum + secondNum);
 }
-// cook(mleko, mouka, vejce)
-// cook();
 
-// if (number>0) {
-    // console.log('pět je větší než nula');
-    // console.log('deset je větší jak pět');}
-// if (name === 'Honza') {
-    // console.log('Jmenuje se Honza');}
+function deduct(firstNum, secondNum, message) {
+    console.log(message + " " + (firstNum - secondNum));
+}
+
+function compare(firstNum, secondNum) {
+    if (firstNum == secondNum) {
+        console.log(firstNum + " equals " + secondNum);
+    } else {
+        console.log(firstNum + " does not equal " + secondNum);
+    }
+}
+
+// 2 == 2
+// true
+// 2 === 2
+// true
+// 2 == "2"
+// true
+// 2 === "2"
+// false
+// 2 === Number("2")
+// true
